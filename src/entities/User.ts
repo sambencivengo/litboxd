ce3f9d233b5bd5a10bf13996bf53158814440fc4
Sam Bencivengo <sambencivengo@gmail.com>
2023-01-18 11:12:37 -0700

    resolvers started

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,14 +1,18 @@
+import { Field, ID } from 'type-graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
 	@PrimaryGeneratedColumn()
+	@Field(() => ID)
 	id: number;
 
 	@Column({ type: 'varchar', unique: true })
+	@Field()
 	username: string;
 
 	@Column('varchar')
+	@Field()
 	password: string;
 	// TODO: add many to one reviews
 }
---
+++ src/resolvers/user.ts
@@ -0,0 +1,62 @@
+import argon2 from 'argon2';
+import {
+	Arg,
+	Field,
+	InputType,
+	Mutation,
+	ObjectType,
+	Resolver,
+} from 'type-graphql';
+import { User } from '../entities';
+import entityManager from '../entityManager';
+
+@InputType() // Used for arguments
+class UsernamePasswordInput {
+	@Field()
+	username: string;
+	@Field()
+	password: string;
+}
+
+@ObjectType()
+class FieldError {
+	@Field()
+	field: string;
+	@Field()
+	message: string;
+}
+
+@ObjectType() // Used for responses
+class UserResponse {
+	@Field(() => [FieldError], { nullable: true })
+	errors?: FieldError[];
+
+	@Field(() => User, { nullable: true })
+	user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+	@Mutation(() => UserResponse)
+	async register(
+		@Arg('options') options: UsernamePasswordInput
+		// @Ctx TODO: context for apollo
+	): Promise<UserResponse> {
+		const { password, username } = options;
+
+		// TODO: validation
+
+		const hashedPassword = await argon2.hash(password);
+
+		const user = entityManager.getRepository(User).create({
+			username: username.toLowerCase(),
+			password: hashedPassword,
+		});
+
+		try {
+			await user;
+		} catch (error) {}
+
+		return { user };
+	}
+}
