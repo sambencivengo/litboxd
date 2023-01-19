29b290e6886a253cd471ed3a92fa8fe6b4198b40
Sam Bencivengo <sambencivengo@gmail.com>
2023-01-19 12:41:37 -0700

    layout and theming

---
+++ theme/index.ts
@@ -0,0 +1,46 @@
+import { DeepPartial, extendTheme, Theme, ThemeConfig } from '@chakra-ui/react';
+import { mode, StyleFunctionProps } from '@chakra-ui/theme-tools';
+
+const config: ThemeConfig = {
+	initialColorMode: 'system',
+	useSystemColorMode: true,
+};
+
+export const colors = {
+	darkBlue: '#14181c',
+	green: '#00c030',
+	orange: '#ff7d00',
+	greyBlue: '#456',
+	grey: '#abc',
+	deepRed: '#D90429',
+};
+
+export const theme = extendTheme({
+	...config,
+	// components: {
+	// 	Button: {
+	// 		variants: {
+	// 			solid: () => ({
+	//
+	// 			}),
+	// 		},
+	// 	},
+	// 	Input: {
+	// 		variants: {
+	// 			filled: {
+	// 				field: {
+	// 			},
+	// 		},
+	// 	},
+	// },
+	styles: {
+		global: (props: StyleFunctionProps) => ({
+			body: {
+				backgroundColor: mode(colors.greyBlue, colors.darkBlue)(props),
+				color: mode(colors.darkBlue, colors.grey)(props),
+			},
+			'::-webkit-scrollbar': { display: 'none' },
+		}),
+	},
+	colors,
+}) as DeepPartial<Theme>;
