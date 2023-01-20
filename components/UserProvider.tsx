cee94084958a4cffeb859f033d715a37744d3144
Sam Bencivengo <sambencivengo@gmail.com>
2023-01-20 08:36:58 -0700

    context provider for user created

---
+++ components/UserProvider.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { setSourceMapRange } from 'typescript';
+import { SimpleUser } from '../src/types';
+
+interface UserProviderProps {
+	children: React.ReactNode;
+}
+
+interface LoginAndSignUpArgs {
+	username: string;
+	password: string;
+}
+
+interface UserContextData {
+	isLoading: boolean;
+	user: SimpleUser | null;
+	signUp: (a: LoginAndSignUpArgs) => Promise<void>;
+	login: (a: LoginAndSignUpArgs) => Promise<void>;
+}
+
+const UserContext = React.createContext<UserContextData>({
+	user: null,
+	isLoading: false,
+	signUp: async () => {},
+	login: async () => {},
+});
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+	const [isLoading, setIsLoading] = React.useState(false);
+	const [user, setUser] = React.useState<SimpleUser | null>(null);
+
+	// LOG IN
+	const login = async ({
+		username,
+		password,
+	}: LoginAndSignUpArgs): Promise<void> => {
+		try {
+			const res = await fetch('/api/users/register', {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json',
+				},
+				body: JSON.stringify({
+					username,
+					password,
+				}),
+				credentials: 'include',
+			});
+
+			if (!res.ok) {
+				setUser(null);
+				return;
+			}
+
+			const data: SimpleUser = await res.json();
+			setUser(data);
+		} catch (error) {
+			setUser(null);
+			console.error(error);
+		}
+	};
+
+	// SIGN UP
+	const signUp = async ({
+		username,
+		password,
+	}: LoginAndSignUpArgs): Promise<void> => {
+		const res = await fetch('/api/users/login', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({ username, password }),
+			credentials: 'include',
+		});
+
+		if (!res.ok) {
+			setUser(null);
+			return;
+		}
+	};
+
+	return (
+		<UserContext.Provider value={{ login, user, isLoading, signUp }}>
+			{children}
+		</UserContext.Provider>
+	);
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../theme';
 import { Layout } from '../components/Layout/Layout';
+import { UserProvider } from '../components/UserProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<UserProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</UserProvider>
 		</ChakraProvider>
 	);
 }
