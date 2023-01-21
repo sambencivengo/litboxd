2419ed8f0e04db6609b8f144b0ec27ff8290eca3
Sam Bencivengo <sambencivengo@gmail.com>
2023-01-21 00:02:07 -0700

    formik added to book search bar, control is nice and tight '

--- components/BookSearchBar.tsx
+++ components/BookSearchBar.tsx
@@ -1,6 +1,8 @@
 import { Search2Icon } from '@chakra-ui/icons';
 import {
 	Box,
+	Flex,
+	FormControl,
 	HStack,
 	IconButton,
 	Input,
@@ -9,6 +11,7 @@ import {
 	Select,
 	Spinner,
 } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
 import React from 'react';
 import { LIBRARY_SEARCH_URL } from '../constants';
 import { colors } from '../theme';
@@ -53,44 +56,81 @@ export const BookSearchBar: React.FC<BookSearchBarProps> = ({
 
 	return (
 		<Box ml={2}>
-			<HStack>
-				<Select
-					onChange={(e) =>
-						setSearchCategory(
-							e.target.value as SearchBarCategoryProps
-						)
-					}
-					color={colors.white}
-					textAlign="center"
-					bgColor={colors.greyBlue}
-					w="200px"
-					borderRight={0}
-					borderRightRadius={0}
-				>
-					<option value="title">Title</option>
-					<option value="author">Author</option>
-					<option value="text">Text</option>
-					<option value="subject">Subject</option>
-				</Select>
-				<InputGroup right={2}>
-					<Input
-						borderLeftRadius={0}
-						onChange={(e) => setSearchBarInput(e.target.value)}
-					/>
-					<InputRightElement width="3rem">
-						{isLoading ? (
-							<Spinner />
-						) : (
-							<IconButton
-								icon={<Search2Icon />}
-								aria-label="Magnifying glass"
-								h="1.75rem"
-								onClick={!searchBarInput ? null : fetchBooks}
-							/>
-						)}
-					</InputRightElement>
-				</InputGroup>
-			</HStack>
+			<Formik
+				validateOnChange={false}
+				validateOnBlur={false}
+				initialValues={{
+					searchBarInput: '',
+					searchCategory: 'title',
+				}}
+				onSubmit={async () => {
+					const res = await fetch(
+						`${LIBRARY_SEARCH_URL}${searchCategory}=${searchBarInput
+							.split(' ')
+							.join('+')}`
+					);
+					const data = await res.json();
+
+					setIsLoading(false);
+
+					setBookResults(data.docs);
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<Flex dir="row" justifyContent="center">
+							<FormControl id="searchCategory">
+								<Select
+									onChange={(e) =>
+										setSearchCategory(
+											e.target
+												.value as SearchBarCategoryProps
+										)
+									}
+									color={colors.white}
+									textAlign="center"
+									bgColor={colors.greyBlue}
+									w="200px"
+									borderRight={0}
+									borderRightRadius={0}
+								>
+									<option value="title">Title</option>
+									<option value="author">Author</option>
+									<option value="text">Text</option>
+									<option value="subject">Subject</option>
+								</Select>
+							</FormControl>
+							<FormControl id="searchBarInput" w={'5000px'}>
+								<InputGroup>
+									<Input
+										borderLeftRadius={0}
+										onChange={(e) =>
+											setSearchBarInput(e.target.value)
+										}
+									/>
+									<InputRightElement width="3rem">
+										{isLoading ? (
+											<Spinner />
+										) : (
+											<IconButton
+												isDisabled={isSubmitting}
+												icon={<Search2Icon />}
+												aria-label="Magnifying glass"
+												h="1.75rem"
+												onClick={
+													!searchBarInput
+														? null
+														: fetchBooks
+												}
+											/>
+										)}
+									</InputRightElement>
+								</InputGroup>
+							</FormControl>
+						</Flex>
+					</Form>
+				)}
+			</Formik>
 		</Box>
 	);
 };
