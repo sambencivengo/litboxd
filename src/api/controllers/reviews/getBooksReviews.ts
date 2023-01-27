fad2f432b080b54e20fe87ed245f9b93327ea39e
Sam Bencivengo <sambencivengo@gmail.com>
2023-01-27 15:44:54 -0700

    shuffling review results

--- src/api/controllers/reviews/getBooksReviews.ts
+++ src/api/controllers/reviews/getBooksReviews.ts
@@ -1,5 +1,4 @@
 import { Handler } from 'express';
-import { title } from 'process';
 import { Review } from '../../../entities';
 
 export const getBooksReviews: Handler = async (req, res) => {
@@ -7,33 +6,63 @@ export const getBooksReviews: Handler = async (req, res) => {
 	const { userId } = req.session;
 
 	try {
-		const reviews = await req.em.find(
-			Review,
-			{
-				bookWorkKey,
-				user: {
-					id: { $nin: [userId] },
+		let reviews: Review[];
+		if (!userId) {
+			reviews = await req.em.find(
+				Review,
+				{
+					bookWorkKey,
 				},
-			},
 
-			{
-				populate: ['user'],
-				fields: [
-					'author',
-					'bookWorkKey',
-					'cover',
-					'id',
-					'likes',
-					'reviewContent',
-					'rating',
-					'reviewContent',
-					'user.id',
-					'user.username',
-				],
-			}
-		);
+				{
+					populate: ['user'],
+					fields: [
+						'author',
+						'bookWorkKey',
+						'cover',
+						'id',
+						'likes',
+						'reviewContent',
+						'rating',
+						'reviewContent',
+						'user.id',
+						'user.username',
+					],
+				}
+			);
+		} else {
+			reviews = await req.em.find(
+				Review,
+				{
+					bookWorkKey,
+					user: {
+						id: { $nin: [userId] },
+					},
+				},
 
-		res.send(reviews);
+				{
+					populate: ['user'],
+					fields: [
+						'author',
+						'bookWorkKey',
+						'cover',
+						'id',
+						'likes',
+						'reviewContent',
+						'rating',
+						'reviewContent',
+						'user.id',
+						'user.username',
+					],
+				}
+			);
+		}
+
+		// Shuffle results and show only 6
+		const shuffledResults = reviews.sort(() => 0.5 - Math.random());
+		const resultArrayOfReviews = shuffledResults.slice(0, 6);
+
+		res.send(resultArrayOfReviews);
 	} catch (error) {
 		res.status(500).send('Unable to get reviews for this book');
 	}
--- src/api/routes/reviews/index.ts
+++ src/api/routes/reviews/index.ts
@@ -6,6 +6,7 @@ import { bookWorkKey } from './[:bookWorkKey]';
 export const reviews = Router({ mergeParams: true });
 
 reviews.use('/:bookWorkKey', bookWorkKey);
+
 reviews.use(userMiddleware);
 reviews.get('/', Controllers.Reviews.get);
 reviews.post('/', Controllers.Reviews.postOrPut);
